837474aa3c2e818c4f4cf7174ed9b612ae6ff391
Mateus <mateus_raimundo95@email.com>
2022-11-26 18:09:14 -0200

    excluir mensagens

--- src/database/knex/migrations/20221022222556_createMensagens.js
+++
@@ -1,9 +0,0 @@
-exports.up = knex => knex.schema.createTable("mensagens", table => {
-  table.increments("id")
-  table.text("mensagem")
-  table.integer("user_id").references("id").inTable("users").onDelete("CASCADE")
-  table.integer("profissional_id").references("id").inTable("profissionais").onDelete("CASCADE")
-  table.timestamp("created_at").default(knex.fn.now())
-})
-
-exports.down = knex => knex.schema.dropTable("mensagens")
