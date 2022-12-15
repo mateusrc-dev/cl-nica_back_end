b33ecc2e957c530db1338ea50c7943e05fa6b028
Mateus Carvalho <mateus_raimundo95@outlook.com>
2022-12-14 23:09:47 -0300

    update schedulesUserController

--- src/Controllers/SchedulesUserController.js
+++ src/Controllers/SchedulesUserController.js
@@ -5,7 +5,7 @@ class SchedulesUserController {
   async index(request, response) {
     const id_user = request.user.id;
     const schedules = await knex("schedules")
-      .select(["schedules.date", "schedules.time", "schedules.duration", "professionals.name", "schedules.status", "professionals.avatar", "professionals.specialization"])
+      .select(["schedules.date", "schedules.time", "schedules.duration", "schedules.id", "professionals.name", "schedules.status", "schedules.justification", "professionals.avatar", "professionals.specialization"])
       .where({ id_user })
       .innerJoin("professionals", "professionals.id", "schedules.id_professional").orderBy("schedules.date")
 
